178175b09bd799dd1498aa35dd4f492acab8f481
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2021-12-09 11:44:22 -0800

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -4,3 +4,21 @@ import numpy as np
 from keras.models import load_model
 from PIL import Image
 
+path_to_images = "../data/images/"
+dataset_filename = "../data/dataset_pickled.gz"
+model_filename = "../data/model.h5"
+
+model = load_model(model_filename)
+
+def predict(image):
+  
+    test_x = np.asarray(Image.open(test_image_path).convert("L"), dtype="float64") / 255
+    test_x_resized = test_x.reshape(1, 150, 150, 1)
+
+    test_y = model.predict(test_x_resized)
+    flattened_test_y = test_y.flatten()
+    
+    x = flattened_test_y[0]
+    y = flattened_test_y[1]
+    
+    print('Sending data: ' + test_y_to_vicon)
