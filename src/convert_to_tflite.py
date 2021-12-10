959cc55410b5fb74c142954b97563c19e80b5e78
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2021-12-10 11:44:18 -0800

    Added code to convert tf model to tflite model

--- src/convert_to_tflite.py
+++ src/convert_to_tflite.py
@@ -1,1 +1,11 @@
+import tensorflow as tf
 
+model_filename = "../data/model.h5"
+tfmodel_filename = "../data/model.tflite"
+
+converter = tf.lite.TFLiteConverter.from_saved_model(model_filename)
+tflite_model = converter.convert()
+
+# Save the model.
+with open(tfmodel_filename, "wb") as f:
+    f.write(tflite_model)
